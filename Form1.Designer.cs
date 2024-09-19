bcd0e040e2c2d851bb6fcad4cab285444dfc85c3
nhatnam <trannhatnamthpttl@gmail.com>
2024-09-19 14:45:49 +0700

    Text Cong

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -20,20 +20,11 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
+        private void btnText_Click(object sender, EventArgs e)
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            int a = 5;
+            int b = 5;
+            Console.WriteLine(a+b);
         }
-
-        #endregion
     }
 }
